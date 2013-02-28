97ac3dc9c7a234f5710bec4ab89ab2aa9ac04f3e
dfyvece <dfy.vece@gmail.com>
2013-02-27 20:42:17 -0600

    last commit was old copy

--- bluetoothlock.ino
+++ bluetoothlock.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
+// for debugging, switch bluetooth to Serial
+#define output bluetooth
+
 // state variables
 #define LOCKED 0
 #define LOCKING 1
@@ -8,54 +11,9 @@
 
 // This is the passcode sent via bluetooth
 char password[] = "poop";
-// IMPORTANT: set passcode length in Shifter.h
-
-#define len 4
-
-// a pseudo shift register implemented with a circular array
-class Shifter {
-  int startpos;
-  int sz;
-  char vals[len];
-  
-public:
-
-  Shifter() : startpos(0), sz(0) {}
-
-  void put(char c);
-  
-  char* get();
-  
-};
-
-
-void Shifter::put(char c) {
-  if (sz < len) {
-    vals[sz] = c;
-    Serial.print("c ");
-    Serial.println(c);
-    Serial.print("sz ");
-    Serial.println(sz);
-    Serial.print("val[sz] ");
-    Serial.println(vals[sz]);
-    sz++;
-  }
-  else {
-    startpos++;
-    vals[startpos % len] = c;
-  }
-  if (startpos > 1023) startpos = startpos%len;
-}
-
-char* Shifter::get() {
-  char value[len+1];
-  int k = 0;
-  for(int i = startpos; k < len; ++i, ++k)
-    value[k] = vals[i%len];
-  value[len] = '\0';
-  return value;
-}
-
+// IMPORTANT: passcode may NOT be more than 20 characters or
+//    you will get a buffer overflow
+#define len strlen(password)
 
 // setup pins
 #define servopin 9
@@ -71,14 +29,13 @@ Servo locker;
 #define minpulse 650
 #define maxpulse 2250
 SoftwareSerial bluetooth(rxpin,txpin);
-Shifter reg;
 
 // angle values for lock and unlock positions
 #define lock 180
 #define unlock 85
 
 // time before the lock locks, in seconds
-#define MAXCOUNT 3
+#define MAXCOUNT 30
 
 // used to hold the three states (locked, locking, and unlocked)
 int state;
@@ -92,9 +49,42 @@ int lastbuttonstate;
 #define DOWN 1
 #define UP 0
 
+// a pseudo shift register implemented with a circular array
+int startpos;
+int sz;
+char vals[20];
+char value[21];
+
+void put(char c) {
+  if (sz < len) {
+    vals[sz] = c;
+    sz++;
+  }
+  else {
+    vals[startpos % len] = c;
+    startpos++;
+  }
+  if (startpos > 1023) startpos = startpos%len;
+}
+
+void get() {
+  int k = 0;
+  for(int i = startpos; k < len; ++i, ++k) {
+    value[k] = vals[i%len];
+  }
+  value[len] = '\0';
+  Serial.print("value: ");
+  Serial.println(value);
+}
+
+
+/*************************************************
+* IMPLEMENATION STARTS HERE
+*************************************************/
 void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
+  bluetooth.listen();
   
   count = 0;
   
@@ -117,6 +107,8 @@ void loop() {
       locker.write(unlock);
       digitalWrite(redpin, LOW);
       digitalWrite(greenpin, HIGH);
+      while(output.available())    //discard bluetooth
+        output.read();
       buttonstate = digitalRead(buttonpin);
       if (buttonstate == HIGH && buttonstate != lastbuttonstate) {
         Serial.println("Go to LOCKING STATE");
@@ -130,8 +122,11 @@ void loop() {
       digitalWrite(greenpin, LOW);
       holdUnlock();
       if (state == UNLOCKED) return;
+      while (output.available())    //discard bluetooth
+        output.read();
       state = LOCKED;
       Serial.println("Go to LOCKED");
+      output.print("Enter password: ");
       break;
       
     case LOCKED:
@@ -145,16 +140,19 @@ void loop() {
         return;
       }
       lastbuttonstate = buttonstate;
-      if (Serial.available()) {
-        char p = Serial.read();
-        reg.put(p);
+      if (output.available()) {
+        char p = output.read();
+        put(p);
         count++;
-        Serial.println(reg.get());
+        get();
+        Serial.println(value);
         Serial.println(count);
       }
       if (count == len) {
         count = 0;
-        if (reg.get() == password) {
+        Serial.print("Entered password: ");
+        Serial.println(value);
+        if (strcmp(value,password) == 0) {
           Serial.println("Valid password entered");
           bluetooth.println("Unlocking...");
           holdUnlock();
@@ -165,9 +163,9 @@ void loop() {
         else {
           Serial.println("Invalid password");
           bluetooth.print("Invalid password: ");
-          bluetooth.println(reg.get());
+          bluetooth.println(value);
         }
-          
+        output.print("Enter password: ");
       }
       
       break;
@@ -208,4 +206,6 @@ boolean checkDelay(int button, int time) {
   }
   return haspressed;
 }
+
+
 
